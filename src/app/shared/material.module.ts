a864a92d3672c8d2d0efb63ff5e97f1f607e5f41
jherson <rodriguezpaul98@gmail.com>
2024-12-06 00:08:07 -0400

    create a Angular material module for import and export dependencies for simplify code in components

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,9 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+export const MaterialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule
+];
